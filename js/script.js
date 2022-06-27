97c8a9705e153359d8ada715ff7a718fe3f9282e
Neo-Machina <andreistatache1997@gmail.com>
2022-06-27 16:34:20 +0200

    array completed

--- js/script.js
+++ js/script.js
@@ -14,7 +14,21 @@
 var app = new Vue(
     {
         el: '#root',
-        data: 
-
+        data: {
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
     }
 )
